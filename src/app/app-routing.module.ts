18874f5ddc1de886e5a8e08224ab8542ee4c3c89
unknown <muhammadalizkhan>
2023-06-25 00:03:18 +0500

    msg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'catagory', component: SingleCategoryComponent },
+  {path: 'post', component: SinglePostComponent},
+
+  {path: 'about', component: AboutUsComponent},
+  {path: 'term-conditions', component: TermsAndConditionComponent},
+  {path: 'contact', component: ContactUsComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
